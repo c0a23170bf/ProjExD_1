5850027966714e0be9eae5d1c600365492fba2c7
watanabesora <c0a23170bf@edu.teu.ac.jp>
2024-04-16 16:50:30 +0900

    こうかとん

--- ex1-20240416/pygame_image.py
+++ ex1-20240416/pygame_image.py
@@ -10,7 +10,6 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img,True,False)
     kk_img = pg.transform.rotozoom(kk_img,10,1.0)
@@ -18,25 +17,12 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        fonto = pg.font.Font(None,80)
-        txt = fonto.render("hello",True,(255,255,255))
-
-        screen = pg.display.set_mode((800,600))
-        img = pg.image.load("fig/3.png")
-        enn = pg.Surface((20,20))
-        pg.draw.circle(enn,(255,0,0),(10,10),10)
-        txt = fonto.render("hello",True,(255,255,255))
-        screen.blit(img,[300,200])
-        pg.display.update()
-
-        img_rct = img.get_rect()
-        img_rct.center = 300,200
-        screen.blit(img, img_rct)
+            
         screen.blit(bg_img, [0, 0])
+        screen.blit(kk_img, [300,200])
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200)
 
 
 if __name__ == "__main__":
